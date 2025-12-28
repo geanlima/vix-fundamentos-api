89e2b53f60623a042e792b2b19b71a108da65590
Gean Lima <gean.lima@salux.com.br>
2025-12-27 22:54:51 -0300

    commit

--- VoxFundamentos.Infrastructure/obj/Debug/net8.0/VoxFundamentos.Infrastructure.AssemblyInfo.cs
+++ VoxFundamentos.Infrastructure/obj/Debug/net8.0/VoxFundamentos.Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VoxFundamentos.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3e0543fa2a67a4c877fdf6889a4aa102bfeb89c7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+841ce485e0f0077b8b7d3c95d545007f7931e0d6")]
 [assembly: System.Reflection.AssemblyProductAttribute("VoxFundamentos.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VoxFundamentos.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
