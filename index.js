1ae2afb3e594a6c373931a8f003649c8eec71109
Gean Lima <geansilvalima@gmail.com>
2026-04-10 11:45:05 -0300

    commit

---
+++ index.js
@@ -0,0 +1,15 @@
+﻿export default {
+    async fetch(request) {
+        const url = new URL(request.url);
+
+        if (url.pathname === "/") {
+            return Response.json({ mensagem: "API rodando 🚀" });
+        }
+
+        if (url.pathname === "/teste") {
+            return Response.json({ ok: true });
+        }
+
+        return new Response("Not found", { status: 404 });
+    }
+};
